8b3f4931772a517fb7743971dea90b9af998253a
mgrundmo <mgrundmo@gmail.com>
2024-11-18 18:32:04 +0100

    search function completed

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -5,7 +5,7 @@ from . import util
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
+        "entries": util.list_entries(), "header": "All Pages"
     })
 
 def entry(request, title):
@@ -20,15 +20,30 @@ def entry(request, title):
         })
 
 def search(request):
-    # lookup list of entries
+    # lookup list of entries and make lowercase
     entries = util.list_entries()
     entries_lwr = [i.lower() for i in entries]
+
+    # receive search query from form and make lowercase
     query = request.POST.get('q')
-    query = query.lower()
+    # query = query.lower()
 
     # check if list of entries contains query non case sensitive
-    if query in entries_lwr:
+    if query.casefold() in entries_lwr:
         query = util.get_entry(query)
         return render(request, "encyclopedia/entry.html", {
             "title": query, "list_entry": markdown(query)
         })  
+
+    # check if if list of entries contains substring of query
+    elif query:
+        result = [search for search in entries if query.casefold() in search.casefold()]  
+        return render(request, "encyclopedia/index.html", {
+        "entries": result, "header": "Search Results"
+        })
+     
+    # return to index page if query not found    
+    else:
+        return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+        })
